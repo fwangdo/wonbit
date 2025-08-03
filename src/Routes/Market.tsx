39696391e0564f26dae2a21d0a9cfea168d45282
hdy <35fwangdo@gmail.com>
2025-08-03 20:14:19 +0900

    svpt

--- src/Routes/Market.tsx
+++ src/Routes/Market.tsx
@@ -1,3 +1,7 @@
+import { useQuery } from "@tanstack/react-query"; 
+import ApexChart from "react-apexcharts"; 
+import { fetchCoinHistory } from "../api";
+
 function Market() {
     return null; 
 }
