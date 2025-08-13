3e8a22091b068f4ee665cce7289e5f945d5168c7
hdy <35fwangdo@gmail.com>
2025-08-13 15:44:05 +0900

    refactor(Member): Change styeld componenets

--- src/Components/Member.tsx
+++ src/Components/Member.tsx
@@ -1,22 +1,10 @@
 import styled from "styled-components";  
 import React from "react"; 
 
-// export const Col = styled.div`
-//     display: flex;
-//     flex-direction: column;
-//     align-items: center;
-//     justify-content: center; /* 가로 중앙 정렬 */
-//     width: 100%;
-//     margin: 0;
-//     padding: 0;
-//     height: 100%;
-//     margin-top: 150px;
-//     font-size: 50px;
-// `;
 
 export function Col( { children } : { children: React.ReactNode} ) {
     return (
-        <div className="flex flex-col align-center justify-center w-full m-0 p-0 h-1 mt=[150px] text-[50px]">
+        <div className="flex flex-col align-center justify-center w-full m-0 p-0 h-1 mt-[150px] text-[50px]">
             {children}
         </div>
     )
@@ -29,31 +17,27 @@ export const InputStyle = {
     padding: "8px", 
 }
 
-export interface StyledInputProps {
-    marginTop?: string;
+
+export interface StyledInputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+    marginTop?: string
 }
 
-export const StyledInput = styled.input.withConfig({
-    shouldForwardProp: (prop) => prop !== "marginTop" // do not forward to html. 
-})<StyledInputProps>`
-    width: 500px;   // 가로 크기 조절
-    height: 30px;   // 세로 크기 조절
-    padding: 8px; 
-    font-size: 16px;
-    margin-top: ${(props) => props.marginTop || "15px" };
-    border-radius: 10px;
-    border-width: 1px;
-`
+export function StyledInput({ marginTop = "15px", ...props }: StyledInputProps) {
+    return (
+        <input 
+            {...props}
+            className={`w-[500px] h-[30px] p-2 text-[16px] rounded-[10px] border border-solid`}
+            style={{marginTop}}
+        />
+    )
+}
 
-export const StyledBtn = styled.button`
-    width: 500px;
-    height: 40px; 
-    font-size: 16px;
-    padding: 8px;
-    margin-top: 30px;
-    background-color: #3748cf;
-    color: white;
-    border-radius: 10px;
-    border-width: 1px;
-    cursor: pointer;
-`
+
+export function StyledBtn({...props}: React.ButtonHTMLAttributes<HTMLButtonElement>) {
+    return (
+        <button 
+            {...props}
+            className={`w-[500px] h-[40px] text-[16px] p-[8px] mt-[10px] bg-color-[#3748cf] text-white border rounded-[10px] cursor-pointer`}
+        />
+    )
+};
